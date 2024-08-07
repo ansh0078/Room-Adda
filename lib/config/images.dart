4912796a0b59f5d8e23ceb594cbb04e3ceb85a78
ansh0078 <anshraj0078@gmail.com>
2024-08-07 23:01:09 +0530

    added

--- lib/config/images.dart
+++ lib/config/images.dart
@@ -1,11 +1,11 @@
 class AssetsImage {
   static const iconsPath = "assets/icons";
-  static const imagesPath = "assets/image";
+  static const imagesPath = "assets/images";
 
   static const sliderImg1 = "$imagesPath/img_1.png";
   static const sliderImg2 = "$imagesPath/img_2.png";
   static const sliderImg3 = "$imagesPath/img.png";
-  
+
   static const room = "$imagesPath/room.jpg";
 
   static const logo = "$imagesPath/logo.png";
